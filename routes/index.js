701672a2817666962cb88e24ae25e1e28e5a4f91
Mike <michaelmartins.js@hotmail.com>
2024-12-05 11:35:21 -0300

    Add route and parameters

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@
 const express = require('express')
 const routers = express.Router()
 
-const { getUsers } = require("../controllers/controllerGetData") 
+const { getAllUsers, getAllUsersByParameter } = require("../controllers/controllerGetData") 
 
 // Routes
-routers.use("/users", getUsers)
-routers.use("/leitos", (req, resp) => resp.status(200).send("Wellcome leitos page"))
-routers.use("/admin", (req, resp) => resp.status(200).send("Wellcome Admin page"))
+routers.get("/users", getAllUsers)
+routers.get("/users/:parameter", getAllUsersByParameter)
 
 module.exports = routers;
