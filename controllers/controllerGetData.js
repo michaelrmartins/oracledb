d893578f06a9bab62d5ae9c531b896df2cb25b55
Mike <michaelmartins.js@hotmail.com>
2024-12-05 11:38:22 -0300

    Add Get All Users and Get All Users By Parameter

--- controllers/controllerGetData.js
+++ controllers/controllerGetData.js
@@ -1,9 +1,23 @@
 // Controller - GetData 
 
-const getUsers = async ( req, resp) => {
-    resp.status(200).send("Controller get Users")
+const { serviceOracleGetUsers } = require('../service/app-oracleget')
+
+// Return data from all users
+const getAllUsers = async ( req, resp) => {
+    returnUsersData = await serviceOracleGetUsers()
+    const returnUsersDataCpfs = returnUsersData.map(user => user)
+    resp.status(200).send(returnUsersDataCpfs)
+}
+
+// Return data by user parameter
+const getAllUsersByParameter = async ( req, resp) => {
+    returnUsersData = await serviceOracleGetUsers()
+    const userParameter = req.params.parameter
+    const returnUsersDataCpfs = returnUsersData.map(user => user[userParameter])
+    resp.status(200).send(returnUsersDataCpfs)
 }
 
 module.exports = {
-    getUsers
+    getAllUsers,
+    getAllUsersByParameter
 }
